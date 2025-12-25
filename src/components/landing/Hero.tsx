61b858bda2b8c0714c2bbb79a22993f49d525770
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-25 05:24:08 +0000

    remove the purple colour and make it complete black

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -7,7 +7,6 @@ export function Hero() {
   return (
     <section className="relative w-full py-12 md:py-24 lg:py-32 xl:py-48 overflow-hidden">
         <div className="absolute inset-0 bg-grid-white/[0.05] [mask-image:linear-gradient(to_bottom,white_5%,transparent_80%)]"></div>
-        <div className="absolute inset-0 -z-10 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,hsl(var(--primary)/0.3),rgba(255,255,255,0))]"></div>
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:gap-16 items-center">
           <div className="flex flex-col justify-center space-y-4 text-center lg:text-left">
