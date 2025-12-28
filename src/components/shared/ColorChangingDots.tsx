84fe437e20a805f2900fafd6773002bb8e0536e3
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 05:20:41 +0000

    one circle (_for element div_)

--- src/components/shared/ColorChangingDots.tsx
+++ src/components/shared/ColorChangingDots.tsx
@@ -1,9 +1,7 @@
 export function ColorChangingDots() {
   return (
-    <div className="flex space-x-1.5 h-8 w-8 items-center justify-center">
+    <div className="flex h-8 w-8 items-center justify-center">
       <span className="h-2.5 w-2.5 rounded-full animate-color-change-1"></span>
-      <span className="h-2.5 w-2.5 rounded-full animate-color-change-2"></span>
-      <span className="h-2.5 w-2.5 rounded-full animate-color-change-3"></span>
     </div>
   );
 }
