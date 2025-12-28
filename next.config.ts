b9b507c29fb66b4831f85cee8869577d0c43a4c6
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 05:08:31 +0000

    add this gif on the image section

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.corp.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
