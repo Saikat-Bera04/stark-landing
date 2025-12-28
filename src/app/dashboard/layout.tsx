472a5425135d25f4664adae1ea58d9b7cbd62776
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 15:21:07 +0000

    remove it (_for element <BackgroundDots>_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { SidebarDemo } from '@/components/ui/aceternity-sidebar';
-import { BackgroundDots } from "@/components/shared/BackgroundDots";
 import { DashboardHeader } from '@/components/dashboard/DashboardHeader';
 import React from 'react';
 
@@ -15,7 +14,7 @@ export default function DashboardLayout({
       <div className="flex flex-1 flex-col overflow-hidden">
         <DashboardHeader />
         <main className="flex-1 p-4 md:p-6 z-10 relative overflow-y-auto">
-          <BackgroundDots />
+          <div className="absolute inset-0 h-full w-full bg-background [background:radial-gradient(125%_125%_at_50%_10%,#1a1a1a_40%,#007CF0_100%)]"></div>
           <div className="relative z-10">{children}</div>
         </main>
       </div>
