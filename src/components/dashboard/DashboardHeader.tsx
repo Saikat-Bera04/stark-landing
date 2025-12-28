27d23955f4460db13810b6bcc92d154ea38c3a7f
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 15:19:28 +0000

    remove the border that's why its look like a part of sidebar (_for eleme

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -25,7 +25,7 @@ export function DashboardHeader() {
   }, [searchParams]);
 
   return (
-    <header className="flex h-14 items-center border-b bg-neutral-800 px-4 md:px-6">
+    <header className="flex h-14 items-center bg-neutral-800 px-4 md:px-6">
       <div className="flex items-center gap-3">
         <div className="text-primary">{currentView.icon}</div>
         <h1 className="text-lg font-semibold text-foreground">
