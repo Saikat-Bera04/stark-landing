92489f79058be0f00285aff71c5b9561f304238b
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 05:03:57 +0000

    add those on side (_for element div_)

--- src/components/dashboard/SecurityStatus.tsx
+++ src/components/dashboard/SecurityStatus.tsx
@@ -68,8 +68,8 @@ export default function SecurityStatus({ statuses }: SecurityStatusProps) {
       intent="success"
       addon={<Badge variant="outline">ONLINE</Badge>}
     >
-      <div className="flex flex-col">
-        <div className="max-md:order-1 grid grid-cols-3 md:grid-cols-1 gap-4 py-2 px-1 md:max-w-max">
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="grid grid-cols-1 gap-4 py-2 px-1">
           {statuses.map((item, index) => (
             <SecurityStatusItem
               key={index}
@@ -80,12 +80,11 @@ export default function SecurityStatus({ statuses }: SecurityStatusProps) {
             />
           ))}
         </div>
-        <div className="md:absolute md:top-0 md:right-0 w-full md:w-auto md:h-full aspect-square min-[2160px]:right-[10%]">
+        <div className="relative flex-1 aspect-square">
           <Image
             src={securityBotImage.imageUrl}
             alt="Security Status"
-            width={1000}
-            height={1000}
+            fill
             quality={90}
             className="size-full object-contain"
             data-ai-hint={securityBotImage.imageHint}
