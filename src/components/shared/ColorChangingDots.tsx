7966f87fdf3a035dcd3276a853592272f0529ec0
Saikat Bera <work.saikatbera1@gmail.com>
2025-12-28 05:21:35 +0000

    make it big and replicate it foe 4  (_for element div_)

--- src/components/shared/ColorChangingDots.tsx
+++ src/components/shared/ColorChangingDots.tsx
@@ -1,7 +1,11 @@
+
 export function ColorChangingDots() {
   return (
-    <div className="flex h-8 w-8 items-center justify-center">
-      <span className="h-2.5 w-2.5 rounded-full animate-color-change-1"></span>
+    <div className="flex h-8 w-8 items-center justify-center gap-1">
+      <span className="h-3 w-3 rounded-full animate-color-change-1"></span>
+      <span className="h-3 w-3 rounded-full animate-color-change-2"></span>
+      <span className="h-3 w-3 rounded-full animate-color-change-3"></span>
+      <span className="h-3 w-3 rounded-full animate-color-change-4"></span>
     </div>
   );
 }
